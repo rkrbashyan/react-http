f79805bb28e0736e9fc49ceaaeecb73e4050184c
Ruben Krbashyan <krbashyan.r@gmail.com>
2018-03-01 11:37:46 +0200

    axios as http client

--- src/index.js
+++ src/index.js
@@ -3,6 +3,36 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com/';
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+
+// register interseptors globaly
+axios.interceptors.request.use(
+    (request) => {
+        // Edit request
+
+        return request;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+axios.interceptors.response.use(
+    (response) => {
+        // Edit response
+
+        return response;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
